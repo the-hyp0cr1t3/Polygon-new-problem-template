7b194b65163ec48b3dfe1c017d86ec3a7ceda75b
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2020-12-16 21:49:45 +0530

    Update gen_tree.cpp

--- New problem Template/Generators/gen_tree.cpp
+++ New problem Template/Generators/gen_tree.cpp
@@ -4,8 +4,57 @@
 #include <numeric>
 using namespace std;
 
-vector<pair<int, int>> get_edges_random(int n);
-vector<pair<int, int>> get_edges_kary(int n, int k);
+enum InputFormat { Edges, Parents };
+
+template<InputFormat inp>
+auto gen_tree_random(int n) {
+    vector<int> perm(n);
+    iota(perm.begin(), perm.end(), 0);
+    shuffle(perm.begin(), perm.end());
+
+    vector<int> p(n);
+    int elongation = rnd.next(1, n);
+    for(int i = 1; i < n; i++)
+        p[i] = rnd.wnext(i, elongation);
+
+    if constexpr(inp == Parents) { // ----- Parents -----
+        vector<int> par(n, -1);
+        for(int i = 1; i < n; i++)
+            par[perm[i]] = perm[p[i]] + 1;
+        return par;
+    } else { // ----- Edges -----
+        vector<pair<int, int>> edges; edges.reserve(n-1);
+        for(int i = 1; i < n; i++)
+            if(rnd.next(2)) edges.emplace_back(perm[i]+1, perm[p[i]]+1);
+            else edges.emplace_back(perm[p[i]]+1, perm[i]+1);
+        shuffle(edges.begin(), edges.end());
+        return edges;        
+    }
+}
+
+// k = 1 for chain, k = n-1 for star
+template<InputFormat inp>
+auto gen_tree_kary(int n, int k) {
+    vector<int> perm(n);
+    iota(perm.begin(), perm.end(), 0);
+    shuffle(perm.begin(), perm.end());
+
+    if constexpr(inp == Parents) { // ----- Parents -----
+        vector<int> par(n, -1);
+        for(int i = 0, j = 1; j < n; i++, j+=k)
+            for(int id = j; id < min(n, j+k); id++)
+                par[perm[id]] = perm[i] + 1;
+        return par;        
+    } else { // ----- Edges -----
+        vector<pair<int, int>> edges; edges.reserve(n-1);
+        for(int i = 0, j = 1; j < n; i++, j+=k)
+            for(int id = j; id < min(n, j+k); id++)
+                if(rnd.next(2)) edges.emplace_back(perm[i], perm[id]);
+                else edges.emplace_back(perm[id], perm[i]);
+        shuffle(edges.begin(), edges.end());
+        return edges;        
+    }
+}
 
 int main(int argc, char* argv[]) {
     registerGen(argc, argv, 1);
@@ -17,41 +66,8 @@ int main(int argc, char* argv[]) {
     cout << t << endl;
     while(t--) {
         int n = rnd.next(n_min, n_max);
-        auto edges = get_edges_random(n);
+        auto edges = gen_tree_random<Edges>(n);
         for(auto [u, v]: edges)
             cout << u << ' ' << v << endl;
     }
-}
-
-vector<pair<int, int>> get_edges_random(int n) {
-    vector<int> perm(n);
-    iota(perm.begin(), perm.end(), 0);
-    shuffle(perm.begin(), perm.end());
-
-    vector<int> p(n);
-    int elongation = rnd.next(1, n);
-    for(int i = 1; i < n; i++)
-        p[i] = rnd.wnext(i, elongation);
-
-    vector<pair<int, int>> edges; edges.reserve(n-1);
-    for(int i = 1; i < n; i++)
-        if(rnd.next(2)) edges.emplace_back(perm[i]+1, perm[p[i]]+1);
-        else edges.emplace_back(perm[p[i]]+1, perm[i]+1);
-    shuffle(edges.begin(), edges.end());
-    return edges;
-}
-
-// k = 1 for chain, k = n-1 for star
-vector<pair<int, int>> get_edges_kary(int n, int k) {
-    vector<int> perm(n);
-    iota(perm.begin(), perm.end(), 0);
-    shuffle(perm.begin(), perm.end());
-
-    vector<pair<int, int>> edges; edges.reserve(n-1);
-    for(int i = 0, j = 1; j < n; i++, j+=k)
-        for(int id = j; id < min(n, j+k); id++)
-            if(rnd.next(2)) edges.emplace_back(perm[i], perm[id]);
-            else edges.emplace_back(perm[id], perm[i]);
-    shuffle(edges.begin(), edges.end());
-    return edges;
 }
